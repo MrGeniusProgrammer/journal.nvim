822dec04d35db810126611d7165b5397a5f17ab3
MrGeniusProgrammer <mrgeniusprogrammer69@gmail.com>
2024-09-07 22:46:36 +0300

    fix: using journal entry config instead of the root default config

--- lua/journal/config.lua
+++ lua/journal/config.lua
@@ -31,19 +31,19 @@ local function is_dict_like(tbl)
 	return type(tbl) == 'table' and not vim.islist(tbl)
 end
 
-local function convert_strings_to_functions(config, include_keys)
-	for key, value in pairs(config) do
+local function convert_strings_to_functions(entry_config, include_keys)
+	for key, value in pairs(entry_config) do
 		for include_key in include_keys do
 			if key ~= include_key then
 				goto continue
 			end
 		end
 
-		if is_dict_like(config[key]) then
-			convert_strings_to_functions(config[key], include_keys)
+		if is_dict_like(entry_config[key]) then
+			convert_strings_to_functions(entry_config[key], include_keys)
 		else
 			if type(value) == "string" then
-				config[key] = function() return value end
+				entry_config[key] = function() return value end
 			end
 		end
 
@@ -53,7 +53,7 @@ end
 
 M.setup = function(user_config)
 	merge_config(user_config, defaults)
-	convert_strings_to_functions(defaults, { 'template', 'format' })
+	convert_strings_to_functions(defaults.journal, { 'template', 'format' })
 end
 
 return M
